0ac43f76de3c2b5d0517e51682db5bddd293e2e4
menna <mennakhalood01@gmail.com>
2024-08-28 01:24:41 +0300

    Started home screen. Setup firebase. Added popular api.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import '../my_theme_data.dart';
+import 'home_screen_tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'home_screen';
+  List<Widget>tabs=[HomeScreenTab()];
+  // ,SearchTab(),BrowserTab(),WatchTab()
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedTap = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
+    return Scaffold(
+      backgroundColor:MyTheme.primaryColor,
+      bottomNavigationBar:Theme(
+        data:Theme.of(context).copyWith(
+            canvasColor:MyTheme.navigateColor
+        ),
+        child: BottomNavigationBar(
+
+            currentIndex:selectedTap,
+            onTap:(select){
+              selectedTap=select;
+              setState(() {
+
+              });
+            },
+            items:[
+              BottomNavigationBarItem(icon:Icon(Icons.home),label:'HOME',),
+              BottomNavigationBarItem(icon:Icon(Icons.search),label:'SEARCH', ),
+              BottomNavigationBarItem(icon:Icon(Icons.movie_rounded),label: 'BROWSER',),
+              BottomNavigationBarItem(icon:Icon(Icons.checklist),label:'WATCHLIST'),
+            ]),
+      ),
+      body:widget.tabs[selectedTap],
     );
   }
 }
