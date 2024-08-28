f625b4d5c3c0125bd4f612c6e958e97152ae402e
Loujeen <loujeenhazem2003@gmail.com>
2024-08-28 03:35:29 +0300

    search updates

---
+++ lib/Search/search_delegate_view_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:movies_app/api/api_manager.dart';
+import 'package:movies_app/Model/PopularResponse.dart';
+
+class SearchDelegateViewModel extends ChangeNotifier{
+  List<Results>? movieList ;
+  String? errorMessage;
+
+  void getSearch (String query)async{
+    print(query);
+    movieList = null;
+    errorMessage = null;
+    notifyListeners();
+    try{
+      var response = await ApiManage.Search(query: query);
+      if(response?.status_code ==7){
+        errorMessage = response?.status_message;
+      }
+      else{
+        movieList = response?.results;
+      }
+    }
+    catch(e){
+      errorMessage = "Error Loading";
+    }
+    notifyListeners();
+  }
+}
--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -3,8 +3,9 @@ class ApiConstant{
   static const popularUrl='/3/movie/popular';
   static const upComingUrl='/3/movie/upcoming';
   static const topRatedUrl='/3/movie/top_rated';
+  static const SearchUrl ='/3/search/movie';
   // static const discoverUrl='/3/discover/movie';
-  // static const SearchUrl ='/3/search/movie';
+
 
 
 }
