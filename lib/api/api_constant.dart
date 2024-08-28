04b1b780e539fb09acd7a07049200c60c4a29b0d
kmousacodes <139441235+kmousacodes@users.noreply.github.com>
2024-08-29 01:00:51 +0300

    delete api

--- lib/api/api_constant.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstant{
-  static const baseUrl='api.themoviedb.org';
-  static const popularUrl='/3/movie/popular';
-  static const discoverUrl='/3/discover/movie';
-  static const SearchUrl ='/3/search/movie';
-
-
-}
--- lib/api/api_constants.dart
+++
@@ -1,5 +0,0 @@
-class ApiConstants{
-  static const String baseUrl = 'api.themoviedb.org';
-  static const String movieGenreApi = '/3/genre/movie/list';
-  static const String movieDetailsApi = '/3/discover/movie';
-}
